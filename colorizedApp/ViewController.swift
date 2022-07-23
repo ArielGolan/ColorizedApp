27393f89b53a75564eb41db2a3dbc5122f5de6b1
Ariel Golan <ariel.golan.ag@gmail.com>
2022-07-23 18:27:06 +0300

    Code optimisation

--- colorizedApp/ViewController.swift
+++ colorizedApp/ViewController.swift
@@ -13,10 +13,6 @@ class ViewController: UIViewController {
     
     @IBOutlet var colorScreenView: UIView!
     
-    @IBOutlet var redLable: UILabel!
-    @IBOutlet var greenLable: UILabel!
-    @IBOutlet var blueLable: UILabel!
-    
     @IBOutlet var redLableValue: UILabel!
     @IBOutlet var greenLableValue: UILabel!
     @IBOutlet var blueLableValue: UILabel!
@@ -29,82 +25,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorScreenView.layer.cornerRadius = 20
-  
-        setupRedSlider()
-        setupGreenSlider()
-        setupBlueSlider()
-        setupLables()
+        colorScreenView.layer.cornerRadius = 15
+        setColor()
+        setValue(for: redLableValue, greenLableValue, blueLableValue)
 
     }
-
-
-    // MARK: - IB Actions
-
-    @IBAction func redSliderAction() {
-        redLableValue.text = "\(round(redSlider.value * 100) / 100)"
-    
-    }
-
-    @IBAction func greenSliderAction() {
-        greenLableValue.text = "\(round(greenSlider.value * 100) / 100)"
+   
+    @IBAction func rgbSliders(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLableValue.text = string(from: redSlider)
+        case greenSlider:
+            greenLableValue.text = string(from: greenSlider)
+        default:
+            blueLableValue.text = string(from: blueSlider)
+        }
     }
     
-    @IBAction func blueSliderAction() {
-        blueLableValue.text = "\(round(blueSlider.value * 100) / 100)"
-    }
-    
-    @IBAction func setColor() {
-        let red = CGFloat(redSlider.value)
-        let green = CGFloat(greenSlider.value)
-        let blue = CGFloat(blueSlider.value)
-        
-        colorScreenView.backgroundColor = UIColor(red: red,
-                                                  green: green,
-                                                  blue: blue,
-                                                  alpha: 1
+    private func setColor() {
+        colorScreenView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
         )
     }
     
-    
-    
-    
-    // MARK: - Private Methods
-    
-    private func setupLables() {
-        redLable.font = UIFont.systemFont(ofSize: 20)
-        redLable.textColor = .white
-        redLableValue.text = String(redSlider.value)
-         
-        greenLable.font = UIFont.systemFont(ofSize: 20)
-        greenLable.textColor = .white
-        greenLableValue.text = String(greenSlider.value)
+    private func setValue(for lables: UILabel...) {
+        lables.forEach { lable in
+            switch lable {
+            case redLableValue:
+                redLableValue.text = string(from: redSlider)
+            case greenLableValue:
+                greenLableValue.text = string(from: greenSlider)
+            default:
+                blueLableValue.text = string(from: blueSlider)
+            }
+        }
         
-        blueLable.font = UIFont.systemFont(ofSize: 20)
-        blueLable.textColor = .white
-        blueLableValue.text = String(blueSlider.value)
     }
     
-    private func setupRedSlider() {
-        redSlider.value = 0
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
-        redSlider.minimumTrackTintColor = .red
+    private func string(from slider: UISlider) -> String {
+    String(format: "%.2f", slider.value)
+        
     }
-    
-    private func setupGreenSlider() {
-        greenSlider.value = 0
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        greenSlider.minimumTrackTintColor = .green
-    }
-    
-    private func setupBlueSlider() {
-        blueSlider.value = 0
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
-        blueSlider.minimumTrackTintColor = .blue
-    }
-   
 }
+
 
